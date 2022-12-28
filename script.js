3f1a9ecbd2ee5cba36c40c37134ed15237ebbee5
kevinqhpham <kevin617.pham@gmail.com>
2022-12-28 15:11:48 -0600

    added button functionality

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-function grid(){  
-    var columns = 16;
-    var rows = 16;
+var size = 0;
+const button = document.querySelector("#btn");
+    button.addEventListener('click', () => {
+        size = prompt("Enter size for dimensions of drawing board (MAX 100)");
+        grid();
+        draw();
+});
 
+function grid(){  
     var grid = document.createElement('div');
     grid.className = 'grid';
-    for (var i = 0; i < columns; ++i) {
+    for (var i = 0; i < size; ++i) {
         var column = document.createElement('div'); // create column
         column.className = 'column';
-        for (var j = 0; j < rows; ++j) {
+        for (var j = 0; j < size; ++j) {
             var row = document.createElement('div'); // create row
             row.className = 'row';
             column.appendChild(row); // append row in column
@@ -16,14 +21,15 @@ function grid(){
     }
     document.body.appendChild(grid);
 }
-grid();
 
 function draw(){
     const row = document.querySelectorAll(".row");
     row.forEach((item) => {
-        item.addEventListener('click', () => {
+        item.addEventListener('mousedown', () => {
             item.style.backgroundColor = 'black';
         });
     });
 }
-draw();
+
+
+
